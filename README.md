ae3d9cba319fe672fb678d8cad020bccdad3e41c
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-01 19:07:35 +0300

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Tailwind + Vite
+
+This modern website with sleek parallax effects and bento box layouts
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello, Saule&#10084;</h1>
+    </>
+  );
+};
+
+export default App;
